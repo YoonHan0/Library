6d5b04f40aa1c665f15e2bd4e532af972fba108d
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-09-02 17:16:15 +0900

    create function 구현을 위한 component 생성 및 처리 작업

---
+++ React/components/CreateContent.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+/** article에 대한 Component */
+class CreateContent extends Component {
+    render() {
+      return (
+        <article>
+          <h2>Create</h2>
+          
+        </article>
+      );
+    }
+}
+
+export default CreateContent;
