e9bb4ef1a9d94840b3bdf360b4ac7e7a0e8cc42f
HanYoung Yoon <87405950+YoonHan0@users.noreply.github.com>
2022-09-03 19:33:09 +0900

    Make a create function( use push )
    push를 사용해서 contents에 값을 추가함 -> state값을 추가할 때는 push보다 concat을 사용하는 것이 좋음
    push : 원래의 값을 직접적으로 변경함
    concat : 원본에서 값을 추가한 새로운 값을 반환함(원본은 유지됨)

--- React/components/CreateContent.js
+++ React/components/CreateContent.js
@@ -1,12 +1,33 @@
 import { Component } from 'react';
+/*  form태그의 action="" 은 form에서 작업을 수행하고 어디 페이지로 넘어갈지를 정함
+    method=""는 post방식, get방식 중에 어떤 식으로 정보를 넘길지를 정함
+    onSubmit={}는 html의 form태그의 기본 형식인데 submit을 하게되면 어떤 함수를 실행할지*/
 
 /** article에 대한 Component */
 class CreateContent extends Component {
     render() {
       return (
         <article>
-          <h2>Create</h2>
-          
+          <h2>Create</h2> 
+          <form action="/create_process" method="post" 
+            onSubmit={function(e){
+              e.preventDefault();
+              alert('Submit!!');
+              this.props.onSubmit(
+                e.target.title.value,
+                e.target.desc.value
+              );
+            }.bind(this)}>
+            <p>
+              <input type="text" name="title" placeholder="title"></input>
+            </p>
+            <p>
+              <textarea name="desc" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit"></input>
+            </p>
+          </form>
         </article>
       );
     }
